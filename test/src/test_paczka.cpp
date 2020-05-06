d83ff37411232c4d979816e92a9c79b5bee09dda
Bartomiej Piktel <b.piktel@stud.elka.pw.edu.pl>
2020-05-06 22:02:01 +0200

    f

--- test/src/test_paczka.cpp
+++ test/src/test_paczka.cpp
@@ -1,15 +1,3 @@
 #include "gtest/gtest.h"
 
 #include "paczka.h"
-
-TEST(PaczkaTest, Sample) {
-  EXPECT_EQ(1 + 1, 2);
-}
-
-TEST(PaczkaTest, Sample2) {
-  EXPECT_EQ(5, 5);
-}
-
-TEST(PaczkaTest, Sample3) {
-  EXPECT_EQ(125, 125);
-}
