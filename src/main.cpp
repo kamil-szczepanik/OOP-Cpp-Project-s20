ee9b2267bad024afb8d18c8c9bc32c3d3a7dc32e
Karol Stepanienko <k.stepanienko@stud.elka.pw.edu.pl>
2020-05-28 21:17:55 +0200

    Kilka dodatkowych testów w main i uzyteczne makro.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 //#include "IdNumbers.h"
 #include "Package.h"
 
+#define p(x) cout << #x << " ("<< sizeof(x) << ") = "  << x <<endl;
+
 using namespace std;
 
 int main(int, char**) {
@@ -25,6 +27,7 @@ int main(int, char**) {
     //Package *w;
     //OversizePackage package1;
 
+
     double t[3] = {1, 3, 3};
     string a = "A";
     string b = "B";
@@ -35,7 +38,10 @@ int main(int, char**) {
 
     cout << any -> getPackageInfo() << endl;
 
+ 
+
     //w = &package1;
+
 
     cout << sp.getSize() << endl;
 
@@ -43,8 +49,23 @@ int main(int, char**) {
     Package* q = makePackage(1, 5, x, a, "addr1", b, "ttt");
     cout << q -> getPackageInfo() << endl;
     cout << q -> getSize() << endl;
+
+    Package* pc = makePackage(2, 5000, x, "sender", "add", "rec", "radd");
+
+    vector<Package*> v;
+    v.push_back(pc);
+    v.push_back(q);
+
+    cout << "Should be oversized package:" << endl;
+    cout << pc -> getPackageInfo() << endl;
+    cout << pc -> getWeight() << endl;
+    p(pc -> getSize());
+
+    p(v[0] -> getPackageInfo());
+    p(v[1] -> getPackageInfo());
     
     delete q;
+    system("pause");
 
     return 0;
 }
